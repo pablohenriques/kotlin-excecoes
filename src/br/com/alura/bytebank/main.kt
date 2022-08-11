9242b84fb2ce5a1f236130f136fa6e925436c9c8
sousa <ph.soft.servicos@gmail.com>
2022-08-10 23:39:23 -0300

    Configuração inicial do projeto adicionada

---
+++ src/br/com/alura/bytebank/main.kt
@@ -0,0 +1,23 @@
+package br.com.alura.bytebank
+
+import br.com.alura.bytebank.teste.testaAny
+
+fun main() {
+    println("inicio main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1() {
+    println("inicio funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("inicio funcao2")
+    for(i in 1..5) {
+        println(i)
+    }
+    println("fim funcao2")
+}
