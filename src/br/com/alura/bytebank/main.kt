e52f980a6ac0f5113026ba2853e5917b9fa25df2
sousa <ph.soft.servicos@gmail.com>
2022-08-12 22:43:53 -0300

    Tratando Exceptions

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,11 +1,41 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.teste.testaAny
+import java.lang.NumberFormatException
 
 fun main() {
-    println("inicio main")
-    funcao1()
-    println("fim main")
+    val entrada: String = "1.9"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor inválido")
+    }
+
+    // println("inicio main")
+    // val entrada: String = "1,9"
+//    try {
+//        val valor: Double = entrada.toDouble()
+//        println("valor recebido: $valor")
+//    } catch (e: NumberFormatException) {
+//        println("Problema na conversão")
+//        e.printStackTrace()
+//    }
+    // funcao1()
+    // println("fim main")
 }
 
 fun funcao1() {
@@ -17,6 +47,14 @@ fun funcao1() {
 fun funcao2() {
     println("inicio funcao2")
     for(i in 1..5) {
+        try {
+            val divisao = i / 0
+            println("Divisão: $divisao")
+        }
+        catch (e: ArithmeticException) {
+            println("Não é possível dividir por zero.")
+            e.printStackTrace()
+        }
         println(i)
     }
     println("fim funcao2")
